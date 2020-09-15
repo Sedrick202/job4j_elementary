e0f6bc5d24818aa973ab714caffdf44e5cf75c39
Nikita Eltsov <wendris202@mail.ru>
2020-09-15 03:05:23 -0700

    3.3. Разбор первой программы. [#358606]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Eltsov");
+        System.out.println("16.04.1994");
+
+    }
+}
