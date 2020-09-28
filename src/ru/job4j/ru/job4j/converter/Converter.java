893e85b7d9004e7aaee1e433b13f302328df096e
Nikita Eltsov <wendris202@mail.ru>
2020-09-27 23:45:40 -0700

    5. Конвертер валюты [#358586]

---
+++ src/ru/job4j/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 90;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 80; /* формула перевода рублей в доллары. */
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(70);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("70 rubles are " + dollar + " dollar.");
+    }
+
+}
