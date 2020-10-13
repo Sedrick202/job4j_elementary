80b5f0a804aa8156fa97c4d980cab70f5c145a30
Nikita Eltsov <wendris202@mail.ru>
2020-10-13 00:22:45 -0700

    1. Модульные тесты. [#358619]update with test 0

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -13,11 +13,26 @@ public class ConverterTest {
     }
 
     @Test
+    public void whenConvert0RblThen1Euro() {
+        int firstPoint = 0;
+        int expectedOne = 1;
+        int out = Converter.rubleToEuro(firstPoint);
+        Assert.assertEquals(expectedOne, out);
+    }
+
+    @Test
     public void whenConvert80RblThen1Dlr() {
         int secoundPoint = 80;
         int expectedTwo = 1;
         int outTwo = Converter.rubleToDollar(secoundPoint);
         Assert.assertEquals(expectedTwo, outTwo);
+    }
 
+    @Test
+    public void whenConvert0RblThen1Dlr() {
+        int secoundPoint = 0;
+        int expectedTwo = 1;
+        int outTwo = Converter.rubleToDollar(secoundPoint);
+        Assert.assertEquals(expectedTwo, outTwo);
     }
 }
