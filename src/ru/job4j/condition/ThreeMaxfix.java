fd4294132848fef323bfefd03337c8f9720b56d3
Nikita Eltsov <wendris202@mail.ru>
2020-10-27 22:43:27 -0700

    4.5. Отладка программы в IDEA [#358535]

--- src/ru/job4j/condition/ThreeMaxfix.java
+++ src/ru/job4j/condition/ThreeMaxfix.java
@@ -2,21 +2,19 @@ package ru.job4j.condition;
 
 public class ThreeMaxfix {
     public static int max(int first, int second, int third) {
-        int result = first;
+        int result = third;
         if (first >= second && first >= third) {
             result = first;
         }
         else if (second >= first && second >= third) {
             result = second;
         }
-        else {
-            result = third;
-        }
+
         return result;
     }
 
     public static void main(String[] args) {
-       int result2 = ThreeMaxfix.max(10,10,1);
+        int result2 = ThreeMaxfix.max(10,10,1);
         System.out.println(result2);
     }
 }
