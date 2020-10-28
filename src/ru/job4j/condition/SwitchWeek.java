9faac617bcd311b214277f9cadf08957497e0fdc
Nikita Eltsov <wendris202@mail.ru>
2020-10-27 23:29:54 -0700

    4.6.1. Switch [#358545]

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static void main(String args[]) {
+        int day = 0;
+        switch (day) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Птяница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресение");
+                break;
+            default:
+                System.out.println("Неудовлетворительно");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
