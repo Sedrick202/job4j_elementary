c5dcdd7a2d453b386fa08f96c3db301f6f92f307
Nikita Eltsov <wendris202@mail.ru>
2020-11-10 22:00:08 -0800

    5.4. Протеиновая диета [#358643]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            nik = nik * 2;
+            ivan = ivan * 3;
+            month++;
+
+        }
+        return month;
+    }
+}
+
+
